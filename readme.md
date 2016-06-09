b78569297a4648c7429822fb5fe300f772917bbf
Kyle Phillips (Experis) <v-kyphil@microsoft.com>
2016-06-09 14:08:00 -0700

    Added a config file for a message and email address messages are sent from

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-put subjects and emails separated by newlines in each corresponding text file.
+## Before
+Before using this program, you have to enable "Allow less secure apps" in your google account security settings.
+## How To
+Put your custom message you want sent to all recipients in message.txt.
+Put a newline separated list of email recipient address in emails.txt
+Put a newline separated list of subjects in subjects.txt
+In email_address.txt, put your email address on line 1 and your password on line 2
--- sendEmails.py
+++ sendEmails.py
@@ -2,26 +2,45 @@ import smtplib
 from email.message import Message
 from email.header import Header
 
+
+# Settings for gmail accounts
 server = smtplib.SMTP('smtp.gmail.com', 587)
-myEmail = "your_email@gmail.com"
+
+# Your email address
+email_data = open("email_address.txt").read()
+email_data = email_data.split("\n")
+if len(email_data) < 2:
+    exit("Email not properly set.")
+
+myEmail = email_data[0]
+password = email_data[1]
 
 # Emails
 emailFile = open("emails.txt")
 emailString = emailFile.read()
 emails = emailString.split("\n")
+if len(emails) == 0:
+    exit("No email addresses.")
 
 # Subjects
 subFile = open("subjects.txt")
 subString = subFile.read()
 subjects = subString.split("\n")
+if len(subjects) == 0:
+    exit("No subjects.")
+
+# Message
+mFile = open("message.txt")
+msgString = mFile.read()
+if len(msgString) == 0:
+    exit("No message set")
 
 server.ehlo()
 server.starttls()
 server.ehlo()
 
-server.login("your_email_wo_domain", "your_password")
-
-msgString = "Put your email message here."
+username = myEmail.split("@")[0]
+server.login(username, password)
 
 for x in range(0, len(emails)):
     msg = Message()
